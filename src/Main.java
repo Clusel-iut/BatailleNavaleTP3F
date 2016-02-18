796f26f06acd75d6a4f8050077f1e3145abdc738
cluselm <cluselm@PC-B123-13.iut-valence.fr>
2016-02-18 11:52:41 +0100

    Create a game ready to play

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		
+		Partie bataillenavale= new Partie().Play(); 
+		System.out.println("la partie est prête à être jouer");
+		
+		
+		
+		
+		/*Bateau sousmarin= new Sousmarin();
+		Bateau porteavion= new Porteavion();
+		Bateau torpilleur = new Torpilleur();
+		Bateau croiseur = new Croiseur();
+		Bateau contre-torpilleur = new Contre-torpilleur();
+		*/
+		
+	}
+
+}
