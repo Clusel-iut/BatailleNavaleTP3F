bf478ffa8f3876e085eaa4abbaf06fff165ce4c9
sebastienjean <baz.jean@gmail.com>
2016-03-17 08:10:33 +0100

    Reviewed code, added/removed some TODO

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,18 @@
-// TODO (done) update README.md (in english, what/who)
 
-// TODO (done) write comment
+// TODO  rewrite comment (ask for advice)
 /**
- * It's the main class who call every constructors like BattleshipGame and play. 
+ * It's the main class who call every constructors like BattleshipGame and play.
  */
 public class Main
 {
 
-	// TODO write comment	
-/**
- * Create and begin a new BattleshipGame.
- */
+	/**
+	 * Application's main
+	 * @param args command-line arguments (unused)
+	 */
 	public static void main(String[] args)
 	{
 		new BattleshipGame().play();
-		
-		// TODO (done) fix this (the game has been played...)
-		
-		
-		
-
 	}
 
 }
