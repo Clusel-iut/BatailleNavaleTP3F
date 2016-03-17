d5efebf8e53fd83a6e0156cf343c1b5a6c6fca10
cluselm <cluselm@PC-A213-06.iut-valence.fr>
2016-03-17 12:00:14 +0100

    Create the class Player Create the algorithm for Play and all methods need.

--- src/BattleshipGame.java
+++ src/BattleshipGame.java
@@ -8,27 +8,99 @@
 
 public class BattleshipGame
 {
+	private Rack player1Rack;
+	private Rack player2Rack;
+	private Player player1;
+	private Player player2;
+
 	/**
 	 * Creates a new battleship game, ready to be played (2 racks with randomly
-	 * placed boats.)
+	 * placed boats and 2 players ready to play.)
 	 */
 	public BattleshipGame()
 	{
 		this.player1Rack = new Rack();
 		this.player2Rack = new Rack();
+		this.player1 = new Player();
+		this.player2 = new Player();
 
 	}
 
 	// TODO detail the main algorithm (ask for advice)
 	/**
-	 * Plays the game This method returns While boats of one
-	 * player aren't destroyed, the game continue. Else the name of the player
-	 * who haven't all boats destroyed, is print on the screen with the total
-	 * game time.
+	 * Plays the game.
+	 * current player is player 1
+	 * while <game not finished>
+	 * 	<ask current player where to shoot>
+	 *  <evaluate shot result>
+	 *  if <touched>
+	 *    <evaluate game result>
+	 *  else current player changes
 	 */
 	public void play()
+	                                                                                                                                                                                
 	{
 		System.out.println("la partie est prête à être jouée !");
+		Player currentPlayer = this.player1;
+		while (this.notFinished());
+		{	
+			askShoot(currentPlayer);
+			evaluateShoot();
+			if (this.evaluateShoot()) 
+				{
+				evaluateGame();
+				}	
+			else 
+				{
+				 currentPlayer = this.player2;
+				}
+		}
 		System.out.println("la partie est finie.");
+	}
+
+	/**
+	 * This method watches if all boats of one player are destroyed
+	 * If it's true, this method stops the game
+	 * Else, it does nothing 
+	 * @return true or false
+	 */
+	private boolean evaluateGame()
+	{
+		return false;
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * This method asks to the currentPlayer where he wants to shoot
+	 * @param currentPlayer
+	 */
+	private void askShoot(Player currentPlayer)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * This method watches where the shoot go. 
+	 * If it touches a boat from the other player, the method returns true
+	 * Else, returns false
+	 * @return true or false
+	 */
+	private boolean evaluateShoot()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * This method return true if all boats of one player are destroyed.
+	 * Else, the method returns false
+	 * @return true or false
+	 */
+	private boolean notFinished()
+	{
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
---
+++ src/Player.java
@@ -0,0 +1,19 @@
+
+/**
+ * This class represents 2 players
+ * @author cluselm
+ * 
+ * 
+ */
+public class Player
+{
+
+	/**
+	 * This class create 2 players
+	 */
+	public Player()
+	{
+		
+	}
+	
+}
